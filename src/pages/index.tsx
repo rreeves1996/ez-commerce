ab468c49033912d661d5da142765ced698855abe
Ryan Reeves <reeves0ryan@gmail.com>
2023-02-10 18:38:54 -0800

    Various prisma integration changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] });
-fetch('https://dummyjson.com/products')
-	.then((res) => res.json())
-	.then(console.log);
 export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>EZ-Commerce</title>
 			</Head>
+			<main>
+				<h1>hello</h1>
+			</main>
 		</>
 	);
 }
